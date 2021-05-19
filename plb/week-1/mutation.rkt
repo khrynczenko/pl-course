d01ced19951c0d9bce00896fd0d48755379108f3
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-05-19 18:17:38 +0200

    pracice mutable lists in racket (plb week-1)

--- plb/week-1/mutation.rkt
+++ plb/week-1/mutation.rkt
@@ -9,3 +9,13 @@
 
 (define after_mut2
   (begin (set! c 15) (f 1)))
+
+; regular lists are built using cons
+(define const-list (cons 1 (cons 2 null)))
+(define const-list-alias const-list) ; const-list value is taken here and bound to a name const-list-alias
+(set! const-list (cons 2 (cons 3 null))) ; we bind new value to const-list but const-list aliast already had its own value
+
+; mutable lists are built using mcons
+(define mut-list (mcons 1 (mcons 2 null)))
+(define mut-list-alias mut-list)
+(set-mcar! mut-list 2)
