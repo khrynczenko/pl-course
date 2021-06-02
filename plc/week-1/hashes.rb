8697ecd752073e30713c3810c717f90bb014bc33
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-06-02 19:40:13 +0200

    practice hashes in ruby (plc week-1)

---
+++ plc/week-1/hashes.rb
@@ -0,0 +1,11 @@
+h1 = {}
+h1["xd"] = 1
+h1[2] = 2
+puts h1
+puts "----------------------------"
+print "keys = #{h1.keys}\n"
+print "values = #{h1.values}\n"
+print "size = #{h1.size}\n"
+puts "----------------------------"
+range = (1..10)
+range.each { |x| puts x } 
