b720fef8d47c5cf8b341a78849202040e5e7fcfa
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-06-02 19:04:36 +0200

    practice ruby arrays (plc week-1)

---
+++ plc/week-1/arrays.rb
@@ -0,0 +1,13 @@
+a = [1, 2, 3 ,4]
+last = a[-1] # 4
+first = a[0] # 1
+
+
+a[9] = 10 # fills missing fields with `nil`
+a.size # 10
+
+w = Array.new(100) # w is 100 element array filled with zero
+w.push 1
+w.pop # 1
+
+
